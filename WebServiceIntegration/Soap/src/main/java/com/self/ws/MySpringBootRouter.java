6670c0f4f3d67f93d87dbeac9b45e3e8fd403f2f
guhabiswasavijit <guhabiswas.avijit@gmail.com>
2021-09-09 11:37:11 +0530

    Adding debug log

--- WebServiceIntegration/Soap/src/main/java/com/self/ws/MySpringBootRouter.java
+++ WebServiceIntegration/Soap/src/main/java/com/self/ws/MySpringBootRouter.java
@@ -3,17 +3,19 @@ package com.self.ws;
 import java.io.File;
 import java.io.InputStream;
 import java.nio.file.StandardCopyOption;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import com.self.wsIntegration.types.UploadRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class MySpringBootRouter extends RouteBuilder {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MySpringBootRouter.class);
 	
 	private static String outputDirectory = "";
 	
@@ -27,15 +29,18 @@ public class MySpringBootRouter extends RouteBuilder {
         from("cxf:bean:cxfFileUploader?dataFormat=PAYLOAD&wsdlURL=WSIntegrationDemo.wsdl")
         .process(new Processor() {
 			@Override
-			public void process(Exchange exchange) throws Exception {			
+			public void process(Exchange exchange) throws Exception {
+				LOG.debug("Getting Output Directory:{}",outputDirectory);
 				UploadRequest uploadRequest = exchange.getIn().getBody(UploadRequest.class);
 				String fileName = uploadRequest.getTitle();
 				StringBuilder filePath = new StringBuilder(outputDirectory);
 				filePath.append(fileName);
 				filePath.append(".csv");
 				File targetFile = new File(filePath.toString());
+				LOG.debug("Creating file:{}",filePath.toString());
 				InputStream initialStream = uploadRequest.getFileData().getInputStream();
 				java.nio.file.Files.copy(initialStream, targetFile.toPath(),StandardCopyOption.REPLACE_EXISTING);
+				LOG.debug("Copy file Successful");
 				initialStream.close();
 				
 			}        	
